1c78ed046cefa711eba59327620b41bafbf2d581
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2021-05-28 10:35:30 -0300

    Condições (Parte 1)

---
+++ aula11/ex008.js
@@ -0,0 +1,11 @@
+var ve1 = 12.5;
+
+console.log("A velocidade do seu carro é " + ve1);
+
+if(ve1 > 60) { //condição simples
+    console.log("Você ultrapassou a velocidade permitida: MULTADO!");
+}
+
+console.log("Dirija sempre usando cinto de segurança!");
+
+
---
+++ aula11/ex009.js
@@ -0,0 +1,12 @@
+
+
+
+var pais = "França";
+
+console.log(`Vivendo em ${pais}`);
+
+if (pais == "Brasil") {
+    console.log("Brasileiro!");
+} else {
+    console.log("Estrangeiro!");
+}
