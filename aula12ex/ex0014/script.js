4cee4fcf2a7820544e9e3ff2b378f615ab53491d
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2021-06-03 10:03:06 -0300

    update ex0014 aula12

--- aula12ex/ex0014/script.js
+++ aula12ex/ex0014/script.js
@@ -3,17 +3,18 @@ function carregar() {
     var img = window.document.getElementById("imagem");
     var data = new Date()
     var hora = data.getHours();
+    var minutos = data.getMinutes();
     
-    msg.innerHTML = `Agora são ${hora} horas.`;
+    msg.innerHTML = `Agora são ${hora}h${minutos}.`;
 
     if(hora >= 0 && hora < 12) {
-        img.src = "fotomanha.png";
+        img.src = "foto-manha.png";
         window.document.body.style.background = "yellow";
     } else if (hora >=12 && hora <=18) {
-        img.src = "fototarde.png";
+        img.src = "foto-tarde.png";
         window.document.body.style.background = "orange";
     } else {
-        img.src = "fotonoite.png";
+        img.src = "foto-noite.png";
         window.document.body.style.background = "blue";
     }
 }
